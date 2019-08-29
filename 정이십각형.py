1df2f2ba156177bb8bb3d18c986c35d6facbcbe3
osof0211 <gksckdgm211@naver.com>
2019-08-29 16:17:32 +0900

    for문

---
+++ 정이십각형.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+speed('fastest')
+
+for a in range(100):
+    for b in range(2):
+        forward(100)
+        right(60)
+        forward(100)
+        right(120)
+    left(3.6)
