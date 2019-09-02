81d834a8c6b217d4f5eb4fa9d312953023261fb8
jllgame00 <54460735+jllgame00@users.noreply.github.com>
2019-09-02 15:52:07 +0900

    Add files via upload

---
+++ python-class-master/3번째 수업.py
@@ -0,0 +1,33 @@
+from turtle import *
+speed("fastest")
+for j in range(100):
+    for i in range(3):
+        forward(100)
+        right(120)
+    left(3.6)
+forward(100)
+for j in range(100):
+    for i in range(3):
+        forward(100)
+        right(120)
+    left(3.6)
+back(200)
+for j in range(100):
+    for i in range(3):
+        forward(100)
+        right(120)
+    left(3.6)
+forward(100)
+left(90)
+forward(100)
+for j in range(100):
+    for i in range(3):
+        forward(100)
+        right(120)
+    left(3.6)
+back(200)
+for j in range(100):
+    for i in range(3):
+        forward(100)
+        right(120)
+    left(3.6)
